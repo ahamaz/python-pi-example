01cb3f2e1f19e08195342f9bcb0efaed50fbaaba
ahamaz <ahasanfsm@hotmail.com>
2020-01-06 19:56:17 +0000

    New Brach changes to pi

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 #python-pi-example
+// helo new branch added
 This is a example project to demonstrate the use of git on ubuntu.
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/phyton3
 def main (msg):
+// third change
 //THis is a change in pi itself after pi2
 //Print a Messagej pi2 changes
     print(msg)
+def secondary()
+	print ("New Function")
+
+
 main ("Hello People")
